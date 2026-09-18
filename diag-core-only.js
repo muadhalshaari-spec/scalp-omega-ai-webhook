32d06a563e54818e6ba91e62d192ad90fe4f1474
muadhalshaari-spec <muadh.alshaari@gmail.com>
2026-09-18 17:26:59 +0300

    chore: remove temporary diagnostic files

--- diag-core-only.js
+++
@@ -1,1 +0,0 @@
-import * as m from '../lib/quant-core.js';export default function handler(req,res){res.statusCode=200;res.setHeader('content-type','application/json');res.end(JSON.stringify({ok:true,exports:Object.keys(m)}));}
