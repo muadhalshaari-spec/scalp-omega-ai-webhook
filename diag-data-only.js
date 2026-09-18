47f8316e02185de482b7c290de3cb4c963f7bc2b
muadhalshaari-spec <muadh.alshaari@gmail.com>
2026-09-18 17:27:02 +0300

    chore: remove temporary diagnostic files

--- diag-data-only.js
+++
@@ -1,1 +0,0 @@
-import * as m from '../lib/data-intelligence.js';export default function handler(req,res){res.statusCode=200;res.setHeader('content-type','application/json');res.end(JSON.stringify({ok:true,exports:Object.keys(m)}));}
