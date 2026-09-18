07b69ae5f33aab62f6f4a4bb270be64f5e56cc0d
muadhalshaari-spec <muadh.alshaari@gmail.com>
2026-09-18 16:46:15 +0300

    debug(institutional): isolate diag-structure-only.js

---
+++ diag-structure-only.js
@@ -0,0 +1,1 @@
+import * as m from '../lib/market-structure-engine.js';export default function handler(req,res){res.statusCode=200;res.setHeader('content-type','application/json');res.end(JSON.stringify({ok:true,exports:Object.keys(m)}));}
